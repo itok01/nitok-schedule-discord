87445bd71c20a76c3f8d2781dfa04cc466b239c4
Takuya Igei <28337009+itok01@users.noreply.github.com>
2022-01-08 00:44:47 +0900

    first commit

---
+++ Code.ts
@@ -0,0 +1,52 @@
+// @ts-ignore
+import * as Cheerio from 'cheerio';
+import MyEnv from './MyEnv';
+
+const NITOK_SITE_ORIGIN = 'https://www.okinawa-ct.ac.jp';
+const NITOK_YEARLY_SCHEDULE_URL = 'https://www.okinawa-ct.ac.jp/detail.jsp?id=73493&menuid=14402&funcid=1';
+const NITOK_WEEKLY_SCHEDULE_URL = 'https://www.okinawa-ct.ac.jp/detail.jsp?id=75448&menuid=15310&funcid=1';
+
+const main = () => {
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = spreadsheet.getActiveSheet();
+    let $: Cheerio.CheerioAPI;
+
+    const oldYearlySchedulePdfUrl = sheet.getRange('A1').getValue();
+
+    const yearlyScheduleContents = UrlFetchApp.fetch(NITOK_YEARLY_SCHEDULE_URL).getContentText();
+    $ = Cheerio.load(yearlyScheduleContents);
+    const yearlySchedulePdfUrl = NITOK_SITE_ORIGIN + $('#aja_contents_detail > p:nth-child(2) > a').first().attr('href');
+
+    if (oldYearlySchedulePdfUrl != yearlySchedulePdfUrl) {
+        Logger.log(`New Yearly Schedule URL: ${yearlySchedulePdfUrl}`);
+
+        UrlFetchApp.fetch(MyEnv.discordWebhookUrl, {
+            method: 'post',
+            contentType: 'application/json',
+            payload: JSON.stringify({
+                content: `年間行事予定表が更新されました\n${yearlySchedulePdfUrl}`
+            })
+        });
+
+        sheet.getRange('A1').setValue(yearlySchedulePdfUrl);
+    }
+
+    const oldWeeklySchedulePdfUrl = sheet.getRange('B1').getValue();
+    const weeklyScheduleContents = UrlFetchApp.fetch(NITOK_WEEKLY_SCHEDULE_URL).getContentText();
+    $ = Cheerio.load(weeklyScheduleContents);
+    const weeklySchedulePdfUrl = NITOK_SITE_ORIGIN + $('#aja_contents_detail > p > a').last().attr('href');
+
+    if (oldWeeklySchedulePdfUrl != weeklySchedulePdfUrl) {
+        Logger.log(`New Weekly Schedule URL: ${weeklySchedulePdfUrl}`);
+
+        UrlFetchApp.fetch(MyEnv.discordWebhookUrl, {
+            method: 'post',
+            contentType: 'application/json',
+            payload: JSON.stringify({
+                content: `週の時間割が更新されました\n${weeklySchedulePdfUrl}`
+            })
+        });
+
+        sheet.getRange('B1').setValue(weeklySchedulePdfUrl);
+    }
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# nitok-schedule-discord
+
+沖縄高専の年間行事予定表と時間割を Google Apps Script を使って Discord で購読する Bot
